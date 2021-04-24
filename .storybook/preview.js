fee19eebfe4d973612aaf253b987e665fae1c52d
Dan Pickett <dan.pickett@launchacademy.com>
2021-04-24 15:37:03 -0400

    more work on buttons and storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,24 @@
 import { addParameters } from '@storybook/react';
 import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
 import "../theme/typography/typography.css";
+import colors from "../theme/colors/colors"
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
 
 addParameters({
-  docs: {
-    container: DocsContainer,
-    page: DocsPage,
+  backgrounds: {
+    default: 'default',
+    values: [
+      {
+        name: "default",
+        value: colors.white.hex
+      },
+      {
+        name: "callout",
+        value: colors.secondaryBrand.hex
+      }
+    ]
   },
 });
--- theme/buttons/Button.js
+++ theme/buttons/Button.js
@@ -4,10 +4,18 @@ import PropTypes from "prop-types";
 import "./buttons.css";
 
 const Button = ({
-  text, to, size, secondary,
+  text, to, size, secondary, placement,
 }) => {
+  const modifiers = [];
+  if (secondary) {
+    modifiers.push("button_secondary");
+  }
+  if (placement === "callout") {
+    modifiers.push("callout__button");
+  }
+
   if (to) {
-    return <a className={`button button_size_${size} ${secondary ? "button_secondary" : ""}`} href={to}>{text}</a>;
+    return <a className={`button button_size_${size} ${modifiers.join(" ")}`} href={to}>{text}</a>;
   }
   return null;
 };
@@ -16,6 +24,7 @@ Button.defaultProps = {
   to: undefined,
   size: "md",
   secondary: false,
+  placement: undefined,
 };
 
 Button.propTypes = {
@@ -23,6 +32,7 @@ Button.propTypes = {
   to: PropTypes.string,
   size: PropTypes.oneOf(["xl", "lg", "md", "sm"]),
   secondary: PropTypes.bool,
+  placement: PropTypes.oneOf(["callout", "photo", undefined]),
 };
 
 export default Button;
