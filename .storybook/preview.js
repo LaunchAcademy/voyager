86bb98d5c745523147b61dc569883738cf01130a
Dan Pickett <dan.pickett@launchacademy.com>
2021-04-25 14:35:50 -0400

    beginning of responsive menu

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -26,4 +26,49 @@ addParameters({
 
     ]
   },
+
+  viewport: {
+    viewports: {
+      sm: {
+        name: 'sm',
+        styles: {
+          width: '375px',
+          height: '480px'
+        },
+        type: "mobile"
+      },
+      md: {
+        name: 'md',
+        styles: {
+          width: '768px',
+          height: '640px'
+        },
+        type: "mobile"
+      },
+      lg: {
+        name: 'lg',
+        styles: {
+          width: '1024px',
+          height: '768px'
+        },
+        type: "desktop"
+      },
+      xl: {
+        name: 'xl',
+        styles: {
+          width: '1280px',
+          height: '1024px'
+        },
+        type: "desktop"
+      },
+      "2xl": {
+        name: '2xl',
+        styles: {
+          width: '1536px',
+          height: '1280px'
+        },
+        type: "desktop"
+      }
+    }
+  }
 });
