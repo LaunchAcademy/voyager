e432170661747824bf452ef47a1149311f6ec292
Kaylyn DiCiaccio <kaylyndiciaccio@kaylyns-air.lan>
2021-05-11 18:35:39 -0400

    cleaned up the upcoming course schedule code

--- theme/courseSchedule/CourseSchedule.js
+++ theme/courseSchedule/CourseSchedule.js
@@ -1,32 +1,36 @@
 import React from 'react'
+import Button from "../buttons/Button.js"
 
-const CourseSchedule = ({ cohort, enrollmentEnd, preflightBegins, ignitionBegins, cohortBegins, cohortEnds }) => {
+const CourseSchedule = ({ cohort, enrollmentEnd, preflightBegins, ignitionBegins, cohortBegins, cohortEnds, buttonURL, buttonText }) => {
 
   return (
-    <div>
-      <div>
-        <label>Cohort</label>
-        <h3>{cohort}</h3>
+    <div className="course-schedule">
+      <div className="course-schedule__course_content course-schedule__course_content--two-column">
+        <label className="course-schedule__label">Cohort</label>
+        <h3 className="course-schedule__cohort-small-text">{cohort}</h3>
       </div>
-      <div>
-        <label>Enrollment ends</label>
-        <h4>{enrollmentEnd}</h4>
+      <div className="course-schedule__course_content course-schedule__course_content--two-column">
+        <label className="course-schedule__label">Enrollment ends</label>
+        <h4 className="course-schedule__date">{enrollmentEnd}</h4>
       </div>
-      <div>
-        <label>Preflight begins</label>
-        <h4>{preflightBegins}</h4>
+      <div className="course-schedule__course_content">
+        <label className="course-schedule__label">Preflight begins</label>
+        <h4 className="course-schedule__date">{preflightBegins}</h4>
       </div>
-      <div>
-        <label>Ignition begins</label>
-        <h4>{ignitionBegins}</h4>
+      <div className="course-schedule__course_content"> 
+        <label className="course-schedule__label">Ignition begins</label>
+        <h4 className="course-schedule__date">{ignitionBegins}</h4>
       </div>
-      <div>
-        <label>Cohort begins</label>
-        <h4>{cohortBegins}</h4>
+      <div className="course-schedule__course_content">
+        <label className="course-schedule__label">Cohort begins</label>
+        <h4 className="course-schedule__date">{cohortBegins}</h4>
       </div>
-      <div>
-        <label>Cohort ends</label>
-        <h4>{cohortEnds}</h4>
+      <div className="course-schedule__course_content">
+        <label className="course-schedule__label">Cohort ends</label>
+        <h4 className="course-schedule__date">{cohortEnds}</h4>
+      </div>
+      <div className="course-schedule__course_content course-schedule__course_content--two-column">
+        <Button to={buttonURL} text={buttonText} className="hero__button" size="lg" />
       </div>
     </div>
   )
