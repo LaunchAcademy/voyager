5afa8bdf85b2fbf119a17b0f25c1ce3a3e10494d
Kaylyn DiCiaccio <kaylyndiciaccio@kaylyns-air.lan>
2021-05-12 18:32:19 -0400

    typo

--- theme/courseSchedule/CourseScheduleList.js
+++ theme/courseSchedule/CourseScheduleList.js
@@ -11,11 +11,11 @@ const CourseScheduleList = ({ cohortSchedule }) => {
   let cohortEnds
 
   const cohortScheduleData = cohortSchedule.map((date) => {
-    enrollmentEnds = format(new Date(date.enrollmentEnds), 'MMM dd, yyy')
-    preflightBegins = format(new Date(date.preflightBegins), 'MMM dd, yyy') 
-    ignitionBegins = format(new Date(date.ignitionBegins), 'MMM dd, yyy')
-    cohortBegins = format(new Date(date.cohortBegins), 'MMM dd, yyy')
-    cohortEnds = format(new Date(date.cohortEnds), 'MMM dd, yyy')
+    enrollmentEnds = format(new Date(date.enrollmentEnds), 'MMM dd, yyyy')
+    preflightBegins = format(new Date(date.preflightBegins), 'MMM dd, yyyy') 
+    ignitionBegins = format(new Date(date.ignitionBegins), 'MMM dd, yyyy')
+    cohortBegins = format(new Date(date.cohortBegins), 'MMM dd, yyyy')
+    cohortEnds = format(new Date(date.cohortEnds), 'MMM dd, yyyy')
 
     return (
       <CourseSchedule 
