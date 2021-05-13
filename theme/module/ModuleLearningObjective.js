6e9fa5ce76ee59e826d9cb5a1b235c67ccb259dc
AL6981 <amylynnarrington@gmail.com>
2021-05-13 13:56:09 -0400

    PR feedback refactor

--- theme/module/ModuleLearningObjective.js
+++ theme/module/ModuleLearningObjective.js
@@ -3,17 +3,16 @@ import Icon from "../icons/Icon"
 import PropTypes from "prop-types";
 import "./moduleLearningObjective.css";
 
-const ModuleLearningObjective = ({ iconName, objectiveDescription }) => (
+const ModuleLearningObjective = ({ objectiveDescription }) => (
   <div className="module-learning-objective">
     <div className="module-learning-objective__icon">
-      <Icon className="h-10 w-10" aria-hidden="true" name={iconName} />
+      <Icon className="h-10 w-10" aria-hidden="true" name="info-circle" />
     </div>
     <p className="module-learning-objective__description">{objectiveDescription}</p>
   </div>
 );
 
 ModuleLearningObjective.propTypes = {
-  iconName: PropTypes.string.isRequired,
   objectiveDescription: PropTypes.string.isRequired,
 };
 
