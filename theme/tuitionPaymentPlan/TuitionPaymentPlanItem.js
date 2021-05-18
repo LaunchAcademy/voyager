41088e6bda1b2c3b416aee20f53fbb8a54af6a5a
AL6981 <amylynnarrington@gmail.com>
2021-05-18 10:08:44 -0400

    Updated html elements

--- theme/tuitionPaymentPlan/TuitionPaymentPlanItem.js
+++ theme/tuitionPaymentPlan/TuitionPaymentPlanItem.js
@@ -2,13 +2,13 @@ import React from "react";
 import Button from "../buttons/Button";
 
 const TuitionPaymentPlanItem = ({ title, description, learnMoreUrl }) => {
-  let button = <Button text="Learn More" to={learnMoreUrl} size="lg" className="tuition-payment-plan-item__button" />;
-  learnMoreUrl ? button : null
+  let button;
+  learnMoreUrl ? <Button text="Learn More" to={learnMoreUrl} size="lg" className="tuition-plan__item-button" /> : null
 
   return (
     <div className="tuition-plan__item">
-      <p className="tuition-payment-plan-item__title">{title}</p>
-      <p className="tuition-payment-plan-item__description">{description}</p>
+      <strong className="tuition-plan__item-title">{title}</strong>
+      <p className="tuition-plan__item-description">{description}</p>
       {button}
     </div>
   )
