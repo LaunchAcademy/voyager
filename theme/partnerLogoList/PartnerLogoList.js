04bce7273385c02cc155b3841ba350ed4a5a5316
Dan Pickett <dan.pickett@launchacademy.com>
2021-05-20 22:43:33 -0400

    proptypes and default props for the logo list

--- theme/partnerLogoList/PartnerLogoList.js
+++ theme/partnerLogoList/PartnerLogoList.js
@@ -1,7 +1,8 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./partnerLogoList.css";
 
-const PartnerLogoList = ({ partnerData } = {}) => {
+const PartnerLogoList = ({ partnerData, className } = {}) => {
   const listItems = partnerData.map((partner) => (
     <li className="partner-logo-list__item">
       <img key={partner.name} src={partner.logoImageUrl} alt={partner.name} />
@@ -9,10 +10,19 @@ const PartnerLogoList = ({ partnerData } = {}) => {
   ));
 
   return (
-    <ul className="partner-logo-list">
+    <ul className={`partner-logo-list ${className}`}>
       {listItems}
     </ul>
   );
+};
+
+PartnerLogoList.defaultProps = {
+  className: undefined,
+};
+
+PartnerLogoList.propTypes = {
+  partnerData: PropTypes.arrayOf(PropTypes.object).isRequired,
+  className: PropTypes.string,
 };
 
 export default PartnerLogoList;
