81c78142734b9c5b849f0a339dbcbb37f2b6cef1
Dan Pickett <dan.pickett@launchacademy.com>
2021-05-20 22:42:39 -0400

    banner tweaks

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ import Profile from "./theme/people/Profile.js";
 import TopBar from "./theme/navigation/TopBar.js";
 import BannerTitle from "./theme/banners/BannerTitle.js";
 import SimpleBanner from "./theme/banners/SimpleBanner.js";
+import BannerContent from "./theme/banners/BannerContent.js";
 import TitledParagraph from "./theme/titledParagraph/TitledParagraph.js";
 import Fact from "./theme/factSet/Fact.js";
 import FactSet from "./theme/factSet/FactSet.js";
 import PartnerLogoList from "./theme/partnerLogoList/PartnerLogoList.js";
 
 export {
-  Button, colors, Icon, Profile, TopBar, BannerTitle, SimpleBanner, TitledParagraph, Fact, FactSet, PartnerLogoList,
+  Button, colors, Icon, Profile, TopBar, BannerTitle, SimpleBanner, TitledParagraph, Fact, FactSet, PartnerLogoList, BannerContent,
 };
