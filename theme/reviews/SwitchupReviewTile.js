a49954ad8647e385e06e8ab2c9049183e112a396
Kaylyn <klattk@gmail.com>
2021-05-24 14:13:13 -0400

    made corrections to css and added yarn.lock

--- theme/reviews/SwitchupReviewTile.js
+++ theme/reviews/SwitchupReviewTile.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import ReviewStars from "./ReviewStars.js"
-import "./reviewtiles.css"
+import "./reviewTiles.css"
 
 const SwitchupReviewTile = ({ reviewTitle, reviewedAt, alumniName, alumniTitle, alumniGradYear, overallScore, curriculumScore, jobAssistanceScore, reviewUrl, children }) => {
 
@@ -11,16 +11,16 @@ const SwitchupReviewTile = ({ reviewTitle, reviewedAt, alumniName, alumniTitle, 
   }
 
   return (
-    <article className="review review_switchup">
-      <section className="review_switchup__reviewer">
-        <div className="reviewer__name">
+    <article className="review review__switchup">
+      <section className="review_reviewer__switchup">
+        <div className="reviewer_name">
           <h4>{alumniName}{alumniTitleCheck}</h4>
           <p>{reviewedAt}</p>
         </div>
           <p>18-Week On-Campus JavaScript Immersive Bootcamp</p>
           <p>Graduated: {alumniGradYear}</p>
       </section>
-      <section className="review_switchup__scores">
+      <section className="review_scores__switchup">
         <div>
           <p>Overall Score</p>
           <ReviewStars rating={overallScore}/>
@@ -34,7 +34,7 @@ const SwitchupReviewTile = ({ reviewTitle, reviewedAt, alumniName, alumniTitle, 
           <ReviewStars rating={jobAssistanceScore}/>
         </div>
       </section>
-      <section className="review_switchup__review">
+      <section className="review_narrative__switchup">
         <h4 className="review__title">{reviewTitle}</h4>
           <p className="review__text">{children}</p>
         <a className="review__link" href={reviewUrl}>read more</a>
