eecff9d8b5ea0b3b4e40f2263778e996efa24e70
Kaylyn <klattk@gmail.com>
2021-05-24 19:53:33 -0400

    forgot the stars

--- theme/reviews/ReviewStars.js
+++ theme/reviews/ReviewStars.js
@@ -1,22 +1,16 @@
 import React from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import getIconMap from "../icons/getIconMap";
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 const ReviewStars = ({ rating }) => {
-
-  const iconMap = getIconMap();
-
-  let starsString = []
+ 
+  let starsList = []
   for(let i=0; i < rating; i++) {
-    starsString.push("star") 
+    starsList.push(<li><FontAwesomeIcon key={i} icon={faStar} /></li>)
   }
 
-  const starsArray = starsString.map((star) => {
-    return(<FontAwesomeIcon icon={iconMap[star]} />)
-  })
-
   return(
-    <div className="text-gold">{starsArray}</div>
+    <ul className="review_stars">{starsList}</ul>
   )
 
 }
