cec0bd784c4e6a099a536e43602d67141af14f11
AL6981 <amylynnarrington@gmail.com>
2021-05-26 16:31:12 -0400

    pr feedback incorporated

--- theme/creativeContent/CreativeContent.js
+++ theme/creativeContent/CreativeContent.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const CreativeContent = ({ children }) => (
-  <div>
+  <div className="creative-content__container">
     {children}
   </div>
 );
