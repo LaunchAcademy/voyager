8133316e0b0b7d3a3a3996ca053b1c8e0145fee7
Kaylyn <klattk@gmail.com>
2021-06-14 19:34:57 -0400

    set up blog post

--- theme/blogPosts/BlogPost.js
+++ theme/blogPosts/BlogPost.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
-const BlogPost = ({ blogTitle, authorName, authorImageUrl, socialUrls, tags, children }) => {
+const BlogPost = ({ blogTitle, authorName, authorImageUrl, tags, children }) => {
 
+  
   return(
     <article className="blog" >
       <h1>{blogTitle}</h1>
@@ -10,15 +11,15 @@ const BlogPost = ({ blogTitle, authorName, authorImageUrl, socialUrls, tags, chi
           <img src={authorImageUrl} />
           <div className="profile__name">
             <h4>{authorName}</h4>
-            <p>{postDate}</p>
+           
           </div>
         </div>
         <div className="author__socials">
-        {socialUrls}
+        
         </div>
       </section>
       <section className="blog__content">
-        {children}
+        <p>{children}</p>
       </section>
       <section className="blog__tags">
         {tags}
