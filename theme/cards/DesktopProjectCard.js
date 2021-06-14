ce5034566d55157984dd4c3efd0dc20dd460a6c7
Kaylyn <klattk@gmail.com>
2021-06-14 16:33:38 -0400

    Kaydc/ch5050/success stories copy (#36)
    * fixed quote error
    
    * updated review tiles for content
    
    * hero image + added a student images placeholder
    
    * updated from grid to positioning on read more fade

--- theme/cards/DesktopProjectCard.js
+++ theme/cards/DesktopProjectCard.js
@@ -1,21 +1,33 @@
 import React from "react";
 import DesktopDeviceTile from "../deviceProjectTile/DesktopDeviceTile.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
-const DesktopProjectCard = ({ imageUrl, projectTitle, studentName, studentProfileUrl }) => (
-  <article className="card card__desktop-tile">
-    <section className="card_photo">
-      <DesktopDeviceTile studentProjectScreenshot={imageUrl} />
-    </section>
-    <section className="project-tile_student-info">
-      <figure className="student-info_student-profile">
-        <img src={studentProfileUrl} />
-      </figure>
-      <div className="student-info_title">
-        <h4 className="card_header">{projectTitle}</h4>
-        <h6 className="card_body">{studentName}</h6>
-      </div>
-    </section>
-  </article>
-);
+const DesktopProjectCard = ({ imageUrl, projectLiveUrl, projectTitle, studentName, studentProfileUrl }) => {
+  
+  let studentProfileImage = <FontAwesomeIcon icon={faUser} />
+  if(studentProfileUrl) {
+    studentProfileImage = <img src={studentProfileUrl} />
+  }
+
+  return (
+    <article className="card card__desktop-tile">
+      <section className="card_photo">
+        <DesktopDeviceTile studentProjectScreenshot={imageUrl} />
+      </section>
+      <section className="project-tile_student-info">
+        <figure className="student-info_student-profile">
+          {studentProfileImage}
+        </figure>
+        <div className="student-info_title">
+          <a href={projectLiveUrl}>
+          <h4 className="card_header">{projectTitle}</h4>
+          </a>
+          <h6 className="card_body">{studentName}</h6>
+        </div>
+      </section>
+    </article>
+  );
+} 
 
 export default DesktopProjectCard;
