5e57de679af49fddba1c3a794a85ef907857a6c8
Kaylyn <klattk@gmail.com>
2021-06-16 16:25:49 -0400

    updated width

--- theme/featureList/FeatureListItem.js
+++ theme/featureList/FeatureListItem.js
@@ -4,7 +4,7 @@ import Icon from "../icons/Icon.js";
 const FeatureListItem = ({ title, subtitle, description, iconName }) => (
   <div className="feature-list__item">
     <div className="feature-list__item-icon">
-      <Icon className=" h-10 w-10" aria-hidden="true" name={iconName} />
+      <Icon className="h-10 w-10" aria-hidden="true" name={iconName} />
     </div>
     <div className="feature-list__item-content">
       <strong className="feature-list__item-title">{title}</strong>
