40a195953ed547b7529c24fd042938309ddc10a5
Kaylyn <klattk@gmail.com>
2021-06-16 10:01:53 -0400

    fixed merge mistake

--- theme/cards/DesktopProjectCard.js
+++ theme/cards/DesktopProjectCard.js
@@ -3,21 +3,32 @@ import DesktopDeviceTile from "../deviceProjectTile/DesktopDeviceTile.js";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
-const DesktopProjectCard = ({ imageUrl, projectTitle, studentName, studentProfileUrl }) => (
-  <article className="card card__desktop-tile">
-    <section className="card__photo">
-      <DesktopDeviceTile studentProjectScreenshot={imageUrl} />
-    </section>
-    <section className="project-tile__student-info">
-      <figure className="student-info__student-profile">
-        <img src={studentProfileUrl} />
-      </figure>
-      <div className="student-info__title">
-        <h4 className="card__header">{projectTitle}</h4>
-        <h6 className="card__body">{studentName}</h6>
-      </div>
-    </section>
-  </article>
-);
+const DesktopProjectCard = ({ imageUrl, projectLiveUrl, projectTitle, studentName, studentProfileUrl }) => {
+
+  let studentProfileImage = <FontAwesomeIcon icon={faUser} />
+  if(studentProfileUrl) {
+    studentProfileImage = <img src={studentProfileUrl} />
+  }
+
+  return (
+    <article className="card card__desktop-tile">
+      <section className="card__photo">
+        <DesktopDeviceTile studentProjectScreenshot={imageUrl} />
+      </section>
+      <section className="project-tile__student-info">
+        <figure className="student-info__student-profile">
+          {studentProfileImage}
+        </figure>
+        <div className="student-info__title">
+          <a href={projectLiveUrl}>
+          <h4 className="card__header">{projectTitle}</h4>
+          </a>
+          <h6 className="card__body">{studentName}</h6>
+        </div>
+      </section>
+    </article>
+  );
+}
+
 
 export default DesktopProjectCard;
--- theme/cards/MobileProjectCard.js
+++ theme/cards/MobileProjectCard.js
@@ -10,39 +10,22 @@ const MobileProjectCard = ({ imageUrl, projectLiveUrl, projectTitle, studentName
   }
   return (
     <article className="card card__mobile-tile">
-      <section className="card_photo">
+      <section className="card__photo">
         <MobileDeviceTile studentProjectScreenshot={imageUrl} />
       </section>
-      <section className="project-tile_student-info">
-        <figure className="student-info_student-profile">
+      <section className="project-tile__student-info">
+        <figure className="student-info__student-profile">
           {studentProfileImage}
         </figure>
-        <div className="student-info_title">
+        <div className="student-info__title">
           <a href={projectLiveUrl}>
-          <h4 className="card_header">{projectTitle}</h4>
+          <h4 className="card__header">{projectTitle}</h4>
           </a>
-          <h6 className="card_body">{studentName}</h6>
+          <h6 className="card__body">{studentName}</h6>
         </div>
       </section>
     </article>
   );
 }
-
-const MobileProjectCard = ({ imageUrl, projectTitle, studentName, studentProfileUrl }) => (
-  <article className="card card__mobile-tile">
-    <section className="card__photo">
-      <MobileDeviceTile studentProjectScreenshot={imageUrl} />
-    </section>
-    <section className="project-tile__student-info">
-      <figure className="student-info__student-profile">
-        <img src={studentProfileUrl} />
-      </figure>
-      <div className="student-info__title">
-        <h4 className="card__header">{projectTitle}</h4>
-        <h6 className="card__body">{studentName}</h6>
-      </div>
-    </section>
-  </article>
-);
 
 export default MobileProjectCard;
