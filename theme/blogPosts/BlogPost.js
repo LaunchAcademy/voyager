eac2f29209140abb5115a4fc45cd8fd1622f945b
Kaylyn <klattk@gmail.com>
2021-06-16 11:28:38 -0400

    extracted blog tags

--- theme/blogPosts/BlogPost.js
+++ theme/blogPosts/BlogPost.js
@@ -1,29 +1,10 @@
 import React from 'react'
 
 import Profile from "../people/Profile"
-import Button from "../buttons/Button"
 import "./blogPosts.css"
+import BlogTags from './BlogTags'
 
 const BlogPost = ({ blogTitle, authorName, authorImageUrl, socialUrls, tags, children }) => {
-
-  let tagsSplit
-  if (tags) {
-    tagsSplit = tags.split(",")
-  }
-
-  const tagList = tagsSplit.map((tag) => {
-    return (
-      <li>
-        <Button
-          placement="photo"
-          size="sm"
-          text={tag}
-          to="#"
-        />
-      </li>
-    )
-  
-  })
   
   return(
     <article className="blog" >
@@ -36,9 +17,7 @@ const BlogPost = ({ blogTitle, authorName, authorImageUrl, socialUrls, tags, chi
       </section>
       <section className="blog__tag-container">
         <h6>Tags</h6>
-        <ul className="tag-container__tags">
-          {tagList}
-        </ul>
+        <BlogTags tags={tags} />
       </section>
     </article>
   )
