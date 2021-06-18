21872aa57d9db01824729fdd6f81666e60d6202b
Kaylyn <klattk@gmail.com>
2021-06-17 21:05:54 -0400

    module

--- theme/module/ModuleLearningObjective.js
+++ theme/module/ModuleLearningObjective.js
@@ -6,7 +6,7 @@ import "./moduleLearningObjective.css";
 const ModuleLearningObjective = ({ objectiveDescription }) => (
   <div className="module-learning-objective">
     <div className="module-learning-objective__icon">
-      <Icon className="h-10 w-10" aria-hidden="true" name="info-circle" />
+      <Icon aria-hidden="true" name="info-circle" />
     </div>
     <p className="module-learning-objective__description">{objectiveDescription}</p>
   </div>
