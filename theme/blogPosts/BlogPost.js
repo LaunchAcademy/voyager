5da7843396cb48b559e57f191df1ab559beaf03f
Dan Pickett <dpickett@users.noreply.github.com>
2021-07-23 15:03:51 -0400

    move to rollup
    prepare for publication
    
    package required css
    
    copy images via command line for now
    
    more rollup chain enhancements

--- theme/blogPosts/BlogPost.js
+++ theme/blogPosts/BlogPost.js
@@ -1,26 +1,25 @@
-import React from 'react'
+import React from "react";
 
-import Profile from "../people/Profile"
-import "./blogPosts.css"
-import BlogTags from './BlogTags'
+import Profile from "../people/Profile";
+import "./blogPosts.css";
+import BlogTags from "./BlogTags";
 
-const BlogPost = ({ blogTitle, authorName, authorImageUrl, socialUrls, tags, children }) => {
-  
-  return(
-    <article className="blog" >
-      <h2>{blogTitle}</h2>
-      <section className="blog__author">
-        <Profile name={authorName} headshotUrl={authorImageUrl} socialUrls={socialUrls}/>
-      </section>
-      <section className="blog__content">
-        {children}
-      </section>
-      <section className="blog__tag-container">
-        <h6>Tags</h6>
-        <BlogTags tags={tags} />
-      </section>
-    </article>
-  )
-}
+const BlogPost = ({
+  blogTitle, authorName, authorImageUrl, socialUrls, tags, children,
+}) => (
+  <article className="blog">
+    <h2>{blogTitle}</h2>
+    <section className="blog__author">
+      <Profile name={authorName} headshotUrl={authorImageUrl} socialUrls={socialUrls} />
+    </section>
+    <section className="blog__content">
+      {children}
+    </section>
+    <section className="blog__tag-container">
+      <h6>Tags</h6>
+      <BlogTags tags={tags} />
+    </section>
+  </article>
+);
 
-export default BlogPost
+export default BlogPost;
--- theme/courseSchedule/CourseSchedule.js
+++ theme/courseSchedule/CourseSchedule.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Button from "../buttons/Button.js";
+import "./courseschedule.css";
 
 const CourseSchedule = ({
   cohort,
@@ -11,38 +12,36 @@ const CourseSchedule = ({
   cohortEnds,
   buttonURL,
   buttonText,
-}) => {
-  return (
-    <div className="course-schedule">
-      <div className="course-schedule__course-content course-schedule__course-content_two-column">
-        <label className="course-schedule__label">Cohort</label>
-        <h3 className="course-schedule__cohort">{cohort}</h3>
-      </div>
-      <div className="course-schedule__course-content course-schedule__course-content_two-column">
-        <label className="course-schedule__label">Enrollment ends</label>
-        <h4 className="course-schedule__date">{enrollmentEnds}</h4>
-      </div>
-      <div className="course-schedule__course-content">
-        <label className="course-schedule__label">Preflight begins</label>
-        <h4 className="course-schedule__date">{preflightBegins}</h4>
-      </div>
-      <div className="course-schedule__course-content">
-        <label className="course-schedule__label">Ignition begins</label>
-        <h4 className="course-schedule__date">{ignitionBegins}</h4>
-      </div>
-      <div className="course-schedule__course-content">
-        <label className="course-schedule__label">Cohort begins</label>
-        <h4 className="course-schedule__date">{cohortBegins}</h4>
-      </div>
-      <div className="course-schedule__course-content">
-        <label className="course-schedule__label">Cohort ends</label>
-        <h4 className="course-schedule__date">{cohortEnds}</h4>
-      </div>
-      <div className="course-schedule__course-content course-schedule__course-content_two-column">
-        <Button to={buttonURL} text={buttonText} className="hero__button" size="lg" />
-      </div>
+}) => (
+  <div className="course-schedule">
+    <div className="course-schedule__course-content course-schedule__course-content_two-column">
+      <label className="course-schedule__label">Cohort</label>
+      <h3 className="course-schedule__cohort">{cohort}</h3>
     </div>
-  );
-};
+    <div className="course-schedule__course-content course-schedule__course-content_two-column">
+      <label className="course-schedule__label">Enrollment ends</label>
+      <h4 className="course-schedule__date">{enrollmentEnds}</h4>
+    </div>
+    <div className="course-schedule__course-content">
+      <label className="course-schedule__label">Preflight begins</label>
+      <h4 className="course-schedule__date">{preflightBegins}</h4>
+    </div>
+    <div className="course-schedule__course-content">
+      <label className="course-schedule__label">Ignition begins</label>
+      <h4 className="course-schedule__date">{ignitionBegins}</h4>
+    </div>
+    <div className="course-schedule__course-content">
+      <label className="course-schedule__label">Cohort begins</label>
+      <h4 className="course-schedule__date">{cohortBegins}</h4>
+    </div>
+    <div className="course-schedule__course-content">
+      <label className="course-schedule__label">Cohort ends</label>
+      <h4 className="course-schedule__date">{cohortEnds}</h4>
+    </div>
+    <div className="course-schedule__course-content course-schedule__course-content_two-column">
+      <Button to={buttonURL} text={buttonText} className="hero__button" size="lg" />
+    </div>
+  </div>
+);
 
 export default CourseSchedule;
