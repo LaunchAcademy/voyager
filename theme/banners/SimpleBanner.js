04627b63f7c942349c67b755f8a44b42818443a1
Dan Pickett <dpickett@users.noreply.github.com>
2021-11-27 21:02:43 -0500

    banner tweaks

--- theme/banners/SimpleBanner.js
+++ theme/banners/SimpleBanner.js
@@ -2,11 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./banners.css";
 
-const SimpleBanner = ({ children, backgroundImageUrl, type }) => (
+//note: we are deprecating backgroundImageUrl in
+//favor of backgroundImageProps for responsive images
+const SimpleBanner = ({ children, backgroundImageUrl, backgroundImageProps, type, style = {}}) => (
   <section
     className={`banner__photo banner_${type}`}
-    style={{ backgroundImage: `url("${backgroundImageUrl}")` }}
+    style={{
+      ...style,
+      ...(backgroundImageUrl ? { backgroundImage: `url("${backgroundImageUrl}")` } : {})
+    }}
   >
+
+    {backgroundImageProps &&
+      <div className="banner__background-photo-wrapper">
+        <img {...backgroundImageProps} className="banner__background-photo" />
+      </div>
+    }
     <div className="banner__content">{children}</div>
   </section>
 );
