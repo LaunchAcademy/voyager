1366d0d82cf77767cacf47fccf744b67d94bf344
Dan Pickett <dpickett@users.noreply.github.com>
2022-12-28 17:30:36 -0500

    don't use FC type

--- src/buttons/Button.tsx
+++ src/buttons/Button.tsx
@@ -31,7 +31,11 @@ export const Button = ({
   iconSize = "1x",
   className = "",
   hideText = false,
+<<<<<<< HEAD
 }: ButtonProps): JSX.Element => {
+=======
+}: ButtonProps): JSX.Element | null => {
+>>>>>>> 1a023e7 (don't use FC type)
   const modifiers = []
   if (secondary) {
     modifiers.push("button_secondary")
